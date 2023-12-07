63a25fccc2a1223dd3661a46ea711b83d16db4a8
Alice Corry <alicecorry@icloud.com>
2023-12-07 09:24:29 +0000

    Adding Foundation CSS Framework

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import 'foundation-sites/dist/css/foundation.min.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
