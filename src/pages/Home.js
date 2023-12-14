a314cf9ac36c3e6818e5aa4075ee63a9e50fd1ee
Alice Corry <alicecorry@icloud.com>
2023-12-14 11:41:40 +0000

    Changing styling in Homepage

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,7 +7,7 @@ const Home = () => {
 
     return (
         <div className="grid-container" style={{ marginTop: '20px' }}>
-            <h1>Welcome to <span style={headingStyle}>CollegeHub!</span></h1>
+            <h1>Welcome to <span style={headingStyle}>CollegeHub</span>!</h1>
             <h4><Link to={`/courses`}>Explore our courses</Link>, <Link to={`/lecturers`}>met our lecturers</Link>, and <Link to={`/enrollments`}>and manage your enrollments</Link>.</h4>
             
         </div>
